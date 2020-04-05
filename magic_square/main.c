7417486a81635e15cdc0f989288263b105a0048f
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-05 16:50:27 +0300

    add function that check if a given 2D array is magic square

--- magic_square/main.c
+++ magic_square/main.c
@@ -4,3 +4,59 @@ Author: Liri (5/4/2020)
 Purpose: This file print true if the given array is
 		 magic square, and false otherwise
 \********************************************************/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define SIZE (3)
+#define INITIAL_INDEX (0)
+#define INITIAL_NUMBER (0)
+
+bool check_if_array_is_magic_square(int two_dimension_array[SIZE][SIZE]) {
+	/********************************************************\
+	* Function name - check_if_array_is_magic_square
+	*
+	* Function Purpose - check if given array is magic square
+	*
+	* Parameters - IN int two_dimension_array[SIZE][SIZE] - the given 2D array
+	*			   OUT int - false - if the array is not magic square
+	*						 true - otherwise
+	* Return Value - false - if the array is not magic square
+	*				 true - otherwise
+	*
+	* Side Effects - this function has no side effects
+	*
+	* Semantics - this function check if the sum of each
+	*			  row equal to the sum of each column
+	*
+	* Author - Liri
+	\********************************************************/
+	int row_index = INITIAL_INDEX;
+	int column_index = INITIAL_INDEX;
+	int sum_of_first_row = INITIAL_NUMBER;
+	int sum_of_next_row = INITIAL_NUMBER;
+	int sum_of_next_column = INITIAL_NUMBER;
+
+	/*save the sum of the first row*/
+	for (column_index = INITIAL_INDEX; column_index < SIZE; ++column_index)
+	{
+		sum_of_first_row += two_dimension_array[INITIAL_INDEX][column_index];
+	}
+
+	/*check if the first row is equal to the other rows and columns*/
+	for (row_index = INITIAL_INDEX; row_index < SIZE; ++row_index)
+	{
+		sum_of_next_row = INITIAL_NUMBER;
+		sum_of_next_column = INITIAL_NUMBER;
+		for (column_index = INITIAL_INDEX; column_index < SIZE; ++column_index)
+		{
+			sum_of_next_row += two_dimension_array[row_index][column_index];
+			sum_of_next_column += two_dimension_array[column_index][row_index];
+		}
+		if (sum_of_next_row != sum_of_first_row || sum_of_next_column != sum_of_first_row)
+		{
+			return false;
+		}
+	}
+	return true;
+}
