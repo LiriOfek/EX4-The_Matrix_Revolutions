e783e9dc5e6f56bb46dd9203be07cbccc8323e1d
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-05 16:44:41 +0300

    add main function and documentation

---
+++ magic_square/main.c
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: main.c
+Author: Liri (5/4/2020)
+Purpose: This file print true if the given array is
+		 magic square, and false otherwise
+\********************************************************/
